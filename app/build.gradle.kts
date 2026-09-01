f2bc48437fb95388f777c75802dc04de08fea174
evramantony-source <evramantony@gmail.com>
2026-09-01 09:21:12 +0300

    Add initial app module

---
+++ app/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.evram.androidstudio"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.evram.androidstudio"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+}
