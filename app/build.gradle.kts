ecaa8ff08d035fb41588e90aff33e3864fe23004
evramantony-source <evramantony@gmail.com>
2026-09-03 13:51:02 +0300

    Add Storage Access Framework dependency and bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.evram.androidstudio"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
-        versionName = "0.1.7"
+        versionCode = 9
+        versionName = "0.1.8"
     }
 
     compileOptions {
@@ -23,4 +23,8 @@ android {
     kotlinOptions {
         jvmTarget = "17"
     }
+}
+
+dependencies {
+    implementation("androidx.documentfile:documentfile:1.0.1")
 }
